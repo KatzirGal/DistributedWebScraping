9adb1c7657b29564d7a3da08560fe39fb598395a
Gal Katzir <galkatzir@outlook.com>
2025-03-25 17:05:17 +0200

    start the server

--- WorkerNode/src/WorkerNode.cpp
+++ WorkerNode/src/WorkerNode.cpp
@@ -4,6 +4,6 @@ int main(int argc, char* argv[])
 {
     net::io_context ioc;
     WorkerCore workerCore(ioc, 8000);
+    workerCore.start();
     return 0;
-    //return workerCore.run(argc, argv);
 }
